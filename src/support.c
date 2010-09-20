25950095912bc92b4108de7d04afc6ec31f8fb08
Daniel Fiser <danfis@danfis.cz>
2010-09-20 11:42:13 +0200

    License changed to BSD.

--- src/support.c
+++ src/support.c
@@ -1,3 +1,20 @@
+/***
+ * libgjk
+ * ---------------------------------
+ * Copyright (c)2010 Daniel Fiser <danfis@danfis.cz>
+ *
+ *
+ *  This file is part of libgjk.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
 #include "support.h"
 
 void __gjkSupport(const void *obj1, const void *obj2,
