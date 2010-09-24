5ae33bbbb8e4ee6b0f617f0a9b710188987a3523
Daniel Fiser <danfis@danfis.cz>
2010-09-24 16:58:53 +0200

    MPR: refinement of portal moved to separate function.

--- src/mpr.c
+++ src/mpr.c
@@ -3,6 +3,8 @@
 #include "simplex.h"
 #include "dbg.h"
 
+// TODO: add more explanatory comments.
+
 /** Finds origin (center) of Minkowski difference (actually it can be any
  *  interior point of Minkowski difference. */
 _gjk_inline void findOrigin(const void *obj1, const void *obj2, const gjk_t *gjk,
@@ -12,8 +14,15 @@ _gjk_inline void findOrigin(const void *obj1, const void *obj2, const gjk_t *gjk
  *  origin ray (ray from center of Minkowski diff to (0,0,0).
  *  Returns 0 on success, 1 if origin (0,0,0) is contained in portal and -1
  *  if origin is outside of portal. */
-_gjk_inline int discoverPortal(const void *obj1, const void *obj2,
-                               const gjk_t *gjk, gjk_simplex_t *portal);
+static int discoverPortal(const void *obj1, const void *obj2,
+                          const gjk_t *gjk, gjk_simplex_t *portal);
+
+
+/** Expands portal towards origin and determine if objects intersect.
+ *  Already established portal must be given as argument.
+ *  If intersection is found 0 is returned, -1 otherwise */
+static int refinePortal(const void *obj1, const void *obj2,
+                        const gjk_t *gjk, gjk_simplex_t *portal);
 
 
 /*
@@ -56,10 +65,6 @@ int gjkMPRIntersect(const void *obj1, const void *obj2, const gjk_t *gjk)
 {
     gjk_simplex_t portal;
     int res;
-    gjk_vec3_t v2v1, v3v1, v4v3, v4v0;
-    gjk_vec3_t dir;
-    gjk_real_t dot, dot2;
-    gjk_support_t v4;
 
     // Phase 1: Portal discovery - find portal that intersects with origin
     // ray (ray from center of Minkowski diff to origin of coordinates)
@@ -68,67 +73,14 @@ int gjkMPRIntersect(const void *obj1, const void *obj2, const gjk_t *gjk)
         return (res == 1 ? 1 : 0);
     }
 
+    // TODO: For penetration we need two functions one for collision
+    //       detection and second for obtaining penetration info which can
+    //       be done by expanding portal in correct direction until
+    //       tolerance is reached!
+
     // Phase 2: Portal refinement
-    gjkVec3Sub2(&v2v1, &gjkSimplexPoint(&portal, 2)->v,
-                       &gjkSimplexPoint(&portal, 1)->v);
-    gjkVec3Sub2(&v3v1, &gjkSimplexPoint(&portal, 3)->v,
-                       &gjkSimplexPoint(&portal, 1)->v);
-    while (1){
-        // compute direction outside the portal (from v0 throught v1,v2,v3
-        // face)
-        gjkVec3Cross(&dir, &v2v1, &v3v1);
-
-        // test if origin is inside the portal
-        dot = gjkVec3Dot(&dir, &gjkSimplexPoint(&portal, 1)->v);
-        if (gjkIsZero(dot) || dot > GJK_ZERO)
-            return 1;
-
-        // get next support point
-        __gjkSupport(obj1, obj2, &dir, gjk, &v4);
-
-        gjkVec3Normalize(&dir);
-        dot = GJK_REAL(-1) * gjkVec3Dot(&v4.v, &dir);
-        gjkVec3Sub2(&v4v3, &v4.v, &gjkSimplexPoint(&portal, 3)->v);
-        dot2 = gjkVec3Dot(&v4v3, &dir);
-        if (gjkIsZero(dot) || dot > GJK_ZERO
-                || gjkEq(dot2, gjk->mpr_tolerance)
-                || dot2 < gjk->mpr_tolerance){
-            return 0;
-        }
-
-        gjkVec3Cross(&v4v0, &v4.v, &gjkSimplexPoint(&portal, 0)->v);
-        dot = gjkVec3Dot(&gjkSimplexPoint(&portal, 1)->v, &v4v0);
-        if (dot > GJK_ZERO){
-            dot = gjkVec3Dot(&gjkSimplexPoint(&portal, 2)->v, &v4v0);
-            if (dot > GJK_ZERO){
-                gjkSimplexSet(&portal, 1, &v4);
-                gjkVec3Sub2(&v2v1, &gjkSimplexPoint(&portal, 2)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(&portal, 3)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-            }else{
-                gjkSimplexSet(&portal, 3, &v4);
-                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(&portal, 3)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-            }
-        }else{
-            dot = gjkVec3Dot(&gjkSimplexPoint(&portal, 3)->v, &v4v0);
-            if (dot > GJK_ZERO){
-                gjkSimplexSet(&portal, 2, &v4);
-                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(&portal, 2)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-            }else{
-                gjkSimplexSet(&portal, 1, &v4);
-                gjkVec3Sub2(&v2v1, &gjkSimplexPoint(&portal, 2)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(&portal, 3)->v,
-                                   &gjkSimplexPoint(&portal, 1)->v);
-            }
-        }
-    }
-
-
-    return 0;
+    res = refinePortal(obj1, obj2, gjk, &portal);
+    return (res == 0 ? 1 : 0);
 }
 
 _gjk_inline void findOrigin(const void *obj1, const void *obj2, const gjk_t *gjk,
@@ -139,8 +91,8 @@ _gjk_inline void findOrigin(const void *obj1, const void *obj2, const gjk_t *gjk
     gjkVec3Sub2(&center->v, &center->v1, &center->v2);
 }
 
-_gjk_inline int discoverPortal(const void *obj1, const void *obj2,
-                               const gjk_t *gjk, gjk_simplex_t *portal)
+static int discoverPortal(const void *obj1, const void *obj2,
+                          const gjk_t *gjk, gjk_simplex_t *portal)
 {
     gjk_support_t supp;
     gjk_vec3_t dir, va, vb;
@@ -150,6 +102,9 @@ _gjk_inline int discoverPortal(const void *obj1, const void *obj2,
     findOrigin(obj1, obj2, gjk, &supp);
     if (gjkVec3Eq(&supp.v, gjk_vec3_origin))
         return 1;
+    // TODO: In this case caller will determine penetration information
+    //       from rays casted in directions around sphere
+
 
     // vertex 0 is center of portal
     gjkSimplexSet(portal, 0, &supp);
@@ -170,6 +125,9 @@ _gjk_inline int discoverPortal(const void *obj1, const void *obj2,
                        &gjkSimplexPoint(portal, 1)->v);
     if (gjkVec3Eq(&dir, gjk_vec3_origin))
         return 1;
+    // TODO: 1. If v1 == origin we have touching contact
+    //       2. else we need to build portal to determine penetration info
+
 
     gjkVec3Normalize(&dir);
     __gjkSupport(obj1, obj2, &dir, gjk, gjkSimplexPointW(portal, 2));
@@ -237,4 +195,74 @@ _gjk_inline int discoverPortal(const void *obj1, const void *obj2,
     }
 
     return 0;
+}
+
+static int refinePortal(const void *obj1, const void *obj2,
+                        const gjk_t *gjk, gjk_simplex_t *portal)
+{
+    gjk_vec3_t v2v1, v3v1, v4v3, v4v0;
+    gjk_vec3_t dir;
+    gjk_real_t dot, dot2;
+    gjk_support_t v4;
+
+    gjkVec3Sub2(&v2v1, &gjkSimplexPoint(portal, 2)->v,
+                       &gjkSimplexPoint(portal, 1)->v);
+    gjkVec3Sub2(&v3v1, &gjkSimplexPoint(portal, 3)->v,
+                       &gjkSimplexPoint(portal, 1)->v);
+    while (1){
+        // compute direction outside the portal (from v0 throught v1,v2,v3
+        // face)
+        gjkVec3Cross(&dir, &v2v1, &v3v1);
+
+        // test if origin is inside the portal
+        dot = gjkVec3Dot(&dir, &gjkSimplexPoint(portal, 1)->v);
+        if (gjkIsZero(dot) || dot > GJK_ZERO)
+            return 0;
+
+        // get next support point
+        __gjkSupport(obj1, obj2, &dir, gjk, &v4);
+
+        gjkVec3Normalize(&dir);
+        dot = GJK_REAL(-1) * gjkVec3Dot(&v4.v, &dir);
+        gjkVec3Sub2(&v4v3, &v4.v, &gjkSimplexPoint(portal, 3)->v);
+        dot2 = gjkVec3Dot(&v4v3, &dir);
+        if (gjkIsZero(dot) || dot > GJK_ZERO
+                || gjkEq(dot2, gjk->mpr_tolerance)
+                || dot2 < gjk->mpr_tolerance){
+            return -1;
+        }
+
+        gjkVec3Cross(&v4v0, &v4.v, &gjkSimplexPoint(portal, 0)->v);
+        dot = gjkVec3Dot(&gjkSimplexPoint(portal, 1)->v, &v4v0);
+        if (dot > GJK_ZERO){
+            dot = gjkVec3Dot(&gjkSimplexPoint(portal, 2)->v, &v4v0);
+            if (dot > GJK_ZERO){
+                gjkSimplexSet(portal, 1, &v4);
+                gjkVec3Sub2(&v2v1, &gjkSimplexPoint(portal, 2)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(portal, 3)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+            }else{
+                gjkSimplexSet(portal, 3, &v4);
+                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(portal, 3)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+            }
+        }else{
+            dot = gjkVec3Dot(&gjkSimplexPoint(portal, 3)->v, &v4v0);
+            if (dot > GJK_ZERO){
+                gjkSimplexSet(portal, 2, &v4);
+                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(portal, 2)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+            }else{
+                gjkSimplexSet(portal, 1, &v4);
+                gjkVec3Sub2(&v2v1, &gjkSimplexPoint(portal, 2)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+                gjkVec3Sub2(&v3v1, &gjkSimplexPoint(portal, 3)->v,
+                                   &gjkSimplexPoint(portal, 1)->v);
+            }
+        }
+    }
+
+
+    return -1;
 }
